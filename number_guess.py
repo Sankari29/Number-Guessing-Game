9e1eac427b56ad7cd256674d780b30a89419e796
Sankari29 <141140860+Sankari29@users.noreply.github.com>
2024-10-17 20:54:35 +0530

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,40 +1,66 @@
-from tkinter import *
+import tkinter as tk
 import random
 
-rand=Tk()
-rand.title('NUMBER GUESSING GAME')
-rand.geometry('700x500')
-rand.config(bg='orange')
+class NumberGuessingGame:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Number Guessing Game")
+        self.master.geometry("700x500")
+        self.master.config(bg="light blue")
 
-randNum=random.randint(0,20)
-chance=3
-var=IntVar()
-disp=StringVar()
+        self.rand_num = random.randint(0, 20)
+        self.chances = 3
+        
+        self.var = tk.IntVar()
+        self.message = tk.StringVar()
 
+        self.create_widgets()
 
-def check_guess():
-    global randNum
-    global chance
-    usr_ip=var.get()
-    if chance>0:
-        if usr_ip==randNum:
-            msg=f'YOU WON!! {randNum} is the right answer.'
-        elif usr_ip>randNum:
-            chance=chance-1
-            msg=f'{usr_ip} is greater.You have {chance} attempt only left. '
-        elif usr_ip<randNum:
-            chance=chance-1
-            msg=f'{usr_ip} is smaller.You have {chance} attempt only left'
-        else:
-            msg='Oops...Something went wrong!!'
-    else:
-        msg=f'You lost! keep try man'
-    disp.set(msg)
+    def create_widgets(self):
+        # Title Label
+        title_label = tk.Label(self.master, text="Number Guessing Game (0 to 20)",
+                                font=('Arial', 28, 'bold'), padx=10, pady=10, bg='white')
+        title_label.pack(pady=(10, 0))
 
+        # Entry for User Input
+        self.entry = tk.Entry(self.master, textvariable=self.var, font=('Arial', 20))
+        self.entry.pack(pady=(50, 10))
 
-Label(rand,text='Number Guessing Game from 0 to 20',font=('Arial', 28 , 'bold'),padx=10,pady=10,bg='blue').pack(pady=(10,0))
-Entry(rand,textvariable=var,font=('Arial', 20)).pack(pady=(50,10))
-Button(rand,text='submit',font=('Arial' ,24),command=check_guess).pack()
-Label(rand,textvariable=disp,bg='white',font=('Arial',20)).pack(pady=(20,0))
+        # Submit Button
+        submit_button = tk.Button(self.master, text="Submit", font=('Arial', 24), command=self.check_guess)
+        submit_button.pack()
 
-rand.mainloop()
+        # Message Label
+        self.result_label = tk.Label(self.master, textvariable=self.message, bg='white', font=('Arial', 20))
+        self.result_label.pack(pady=(20, 0))
+
+        # Restart Button
+        restart_button = tk.Button(self.master, text="Restart", font=('Arial', 24), command=self.restart_game)
+        restart_button.pack(pady=(10, 0))
+
+    def check_guess(self):
+        usr_input = self.var.get()
+        if self.chances > 0:
+            if usr_input == self.rand_num:
+                self.message.set(f'YOU WON!! {self.rand_num} is the right answer.')
+                self.chances = 0  # End the game
+            elif usr_input > self.rand_num:
+                self.chances -= 1
+                self.message.set(f'{usr_input} is greater. You have {self.chances} attempts left.')
+            elif usr_input < self.rand_num:
+                self.chances -= 1
+                self.message.set(f'{usr_input} is smaller. You have {self.chances} attempts left.')
+        if self.chances == 0 and usr_input != self.rand_num:
+            self.message.set(f'You lost! The correct number was {self.rand_num}.')
+
+    def restart_game(self):
+        self.rand_num = random.randint(0, 20)
+        self.chances = 3
+        self.var.set(0)  # Reset the entry field
+        self.message.set("")  # Clear the message
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = NumberGuessingGame(root)
+    root.mainloop()
+
